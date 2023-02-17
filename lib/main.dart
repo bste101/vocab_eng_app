809a25e8f60ab30af480f7c082a31d4162da4bee
BSTE101 <naruebet.s@kkumail.com>
2023-02-17 21:41:21 +0700

    remove import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'package:flame/components.dart';
-import 'package:flame/experimental.dart';
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flame/palette.dart';
-import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'package:vocab_eng_app/games/mygame.dart';
 
