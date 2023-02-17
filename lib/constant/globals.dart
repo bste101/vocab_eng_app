37e93549f675cdb6f0b3a1a7aaaa5721bfdf4e3d
teeratep17 <teeratep.p@kkumail.com>
2023-02-17 22:58:36 +0700

    background

--- lib/constant/globals.dart
+++ lib/constant/globals.dart
@@ -1,6 +1,6 @@
 class Globals {
   Globals._();
 
-  static const String iconrocketSprite = 'icon/iconRocket.png';
-  static const String backgroundSprite = 'background/backgroundstartgame.png';
+  static const String iconrocketSprite = 'icon/iconRocketV2.png';
+  static const String backgroundSprite = 'background/backgroundInGame.png';
 }
