27f251404d4bdc39db47bbd0c1a4a93f3548380e
teeratep17 <teeratep.p@kkumail.com>
2023-02-17 22:47:07 +0700

    rocket

--- lib/components/rocket_component.dart
+++ lib/components/rocket_component.dart
@@ -3,16 +3,13 @@ import 'package:vocab_eng_app/constant/globals.dart';
 import 'package:vocab_eng_app/games/mygame.dart';
 
 class RocketComponent extends SpriteComponent with HasGameRef<MyGame> {
-  final double _spriteHeight = 200;
+  //final double _spriteHeight = 200;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
     sprite = await gameRef.loadSprite(Globals.iconrocketSprite);
-    position = gameRef.size / 2;
-    height = _spriteHeight;
-    width = _spriteHeight * 1.42;
-    anchor = Anchor.center;
+    size = gameRef.size;
   }
 }
