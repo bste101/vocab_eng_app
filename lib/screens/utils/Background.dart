d346c0f32d4806ad9c8a636db88ab0a3a3c8900f
BSTE101 <naruebet.s@kkumail.com>
2023-02-17 10:37:56 +0700

    test

--- lib/screens/utils/Background.dart
+++ lib/screens/utils/Background.dart
@@ -1,31 +1,31 @@
-import 'dart:ui';
+// import 'dart:ui';
 
-import 'package:flame/components.dart';
-import 'package:flame/sprite.dart';
-import 'package:vocab_eng_app/screens/BaseWidget.dart';
+// import 'package:flame/components.dart';
+// import 'package:flame/sprite.dart';
+// import 'package:vocab_eng_app/screens/BaseWidget.dart';
 
-class Background extends BaseWidget{
-  SpriteComponent _bgsprite;
+// class Background extends BaseWidget{
+//   SpriteComponent _bgsprite;
 
-  Background(String src) {
-    // ignore: todo
-    // TODO: implement Background
-    throw UnimplementedError();
-  }
+//   Background(String src) {
+//     // ignore: todo
+//     // TODO: implement Background
+//     throw UnimplementedError();
+//   }
 
-  @override
-  void render(Canvas canvas) {
-    _bgsprite.render(canvas);
-  }
+//   @override
+//   void render(Canvas canvas) {
+//     _bgsprite.render(canvas);
+//   }
 
-  @override
-  void resize(Size size) {
-    _bgsprite.width = size.width;
-    _bgsprite.height = size.height;
-  }
+//   @override
+//   void resize(Size size) {
+//     _bgsprite.width = size.width;
+//     _bgsprite.height = size.height;
+//   }
 
-  @override
-  void update() {}
+//   @override
+//   void update() {}
 
 
-}
+// }
